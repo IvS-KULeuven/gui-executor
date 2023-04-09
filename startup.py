784eb7b14d3a35af350e015510f621f3649380be
Rik Huygen <rik.huygen@kuleuven.be>
2023-04-09 20:27:16 +0200

    Added a simple startup.py file

---
+++ startup.py
@@ -0,0 +1,8 @@
+import rich
+from rich import print
+from rich import pretty
+
+# Install automatic pretty printing in the Python REPL
+pretty.install()
+
+print("[blue]Welcome to the examples Task GUI.[/]", flush=True)
