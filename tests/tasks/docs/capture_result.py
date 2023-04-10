65ae6ebefcf2f90f1d14ea1cae79d99da8cbf8ba
Rik Huygen <rik.huygen@kuleuven.be>
2023-04-11 00:10:45 +0200

    Further improvements on the documentation.

---
+++ tests/tasks/docs/capture_result.py
@@ -0,0 +1,14 @@
+from gui_executor.exec import exec_task
+
+UI_MODULE_DISPLAY_NAME = "Capturing function responses"
+
+@exec_task(capture_response=("model", "_", "hexhw"))
+def generate_model(name: str = "LDO -> CSL"):
+
+    print(f"Generating model: {name}")
+
+    model = "This is a model"
+    s = "an unused return value..."
+    hexhw = "this is a reference to a hardware device"
+
+    return model, s, hexhw
